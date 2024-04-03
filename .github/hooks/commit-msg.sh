709944199ee97cc6c7153293be74655ba17c9734
Marius Arlt <marius.arlt@msg.group>
2024-04-03 10:10:32 +0200

    purposely wrong commit message to test the commit-msg hook

--- .github/hooks/commit-msg.sh
+++ .github/hooks/commit-msg.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 commit_regex='^((feat|fix|build|ci|docs|refactor|style|test|perf|chore)(\(\w[-\w]*\))?(: (.*\s*)*))|(Merge (.*\s*)*)|(Revert (.*\s*)*)'
 error_msg="Aborting commit. Your commit message doesn't fit the expected pattern."
